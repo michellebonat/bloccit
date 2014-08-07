0a9a607608a47e937f97c8ff704753e4311cd094
Michelle Bonat <michelle.bonat@gmail.com>
2014-08-06 19:30:45 -0700

    Associated and nested topics and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Bloccit::Application.routes.draw do
   devise_for :users
   resources :posts
+   resources :topics
+
+    resources :topics do
+      resources :posts, except: [:index]
+    end
+    
   get 'about' => 'welcome#about'
 
    root to: 'welcome#index'
