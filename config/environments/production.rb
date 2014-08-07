d648dff1111abc9ab514836152ba1a8bac626499
Michelle Bonat <michelle.bonat@gmail.com>
2014-08-07 09:06:02 -0700

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  # http://bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'mbonat-bloccit.herokuapp.com' }
 end
