334d623b6d5013773900401c4058b38c15910c3e
Michelle Bonat <michelle.bonat@gmail.com>
2014-08-11 11:28:32 -0700

    Images updates with John

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
        flash[:notice] = "User information updated"
        redirect_to edit_user_registration_path
      else
-       render "devise/registrations/edit"
+       redirect_to edit_user_registration_path
      end
    end
  
    private
  
    def user_params
-      params.require(:user).permit(:name, :avatar)
+      params.require(:user).permit(:name, :avatar, :avatar_cache)
    end
  end
