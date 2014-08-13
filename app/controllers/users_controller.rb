ae673b15d5361e1b7e2acff3c17f269adc3211ee
michellebonat <michelle.bonat@gmail.com>
2014-08-13 15:30:06 -0700

    Work with Sasha on image uploads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
    before_action :authenticate_user!
- 
+
    def update
      if current_user.update_attributes(user_params)
        flash[:notice] = "User information updated"
