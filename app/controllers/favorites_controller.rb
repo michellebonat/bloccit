273d903fad5a4b2c1c028ccf200b2ebe369488d6
michellebonat <michelle.bonat@gmail.com>
2014-08-19 15:37:51 -0700

    Favorites third add

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,14 @@ class FavoritesController < ApplicationController
    def create
      @post = Post.find(params[:post_id])
      favorite = current_user.favorites.build(post: @post)
+
+     authorize favorite
  
      if favorite.save
        # Add code to generate a success flash and redirect to @post
        # Remember the path shortcut: [@post.topic, @post]
-       @post.create_favorite
        flash[:notice] = "Your favorite was saved."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      else
        # Add code to generate a failure flash and redirect to @post
        flash[:error] = "There was an error saving your favorite. Please try again."
@@ -19,11 +20,21 @@ class FavoritesController < ApplicationController
    def destroy
    # Get the post from the params
    # Find the current user's favorite with the ID in the params
- 
+     #@topic = Topic.find(params[:topic_id])
+
+     @post = Post.find(params[:post_id])
+     favorite = current_user.favorites.find(params[:id])
+
+     authorize favorite
+
    if favorite.destroy
      # Flash success and redirect to @post
+     flash[:notice] = "This post has been removed from your favorites."
+     redirect_to [@post.topic, @post]
    else
      # Flash error and redirect to @post
+     flash[:error] = "We were unable to unfavorite this post."
+     render :show
    end
  end
 end
