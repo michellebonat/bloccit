40026f7acff6ab17730c2798ccce62d9ccd5e5fa
michellebonat <michelle.bonat@gmail.com>
2014-08-25 15:23:41 -0700

    private-topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,10 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  # updating here for scopes withing scope at bottom of scopes exercise
+  #scope :visible_to, -> (user) { user ? all : where(public: true) }
+  #scope :visible_to, -> (user) { user ? all : where(public: true), user ? current_user : where(public: false) }
+  scope :private, -> { where(public: false) }
+  scope :public, -> { where(public: true) }
+  scope :visible_to, -> (user) { user ? all : where(public: true) }  
 end
+
