ea2d177d771dd1fb815fec4747ab4b758febec1a
michellebonat <michelle.bonat@gmail.com>
2014-08-25 10:03:07 -0700

    Updated favorites controller spec

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -2,33 +2,33 @@
  
  describe FavoritesController do
  
-   include TestFactories
-   include Devise::TestHelpers
+  include TestFactories
+  include Devise::TestHelpers
  
-   before do 
-     @post = associated_post
-     @user = authenticated_user
-     sign_in @user
+  before do 
+    @post = associated_post
+    @user = authenticated_user
+    sign_in @user
    end
- 
+
    describe '#create' do
      it "creates a favorite for the current user and specified post" do
        expect( @user.favorites.find_by_post_id(@post.id) ).to eq(nil)
- 
-       post :create, { post_id: @post.id }
- 
+
+       post :create, { post_id: @post.id, topic_id: @post.topic.id }
+
        expect( @user.favorites.find_by_post_id(@post.id).class ).to eq(Favorite)
      end
    end
- 
+
    describe '#destroy' do
      it "destroys the favorite for the current user and post" do
        favorite = @user.favorites.where(post: @post).create
        expect( @user.favorites.find_by_post_id(@post.id).class ).to eq(Favorite)
- 
-       delete :destroy, { post_id: @post.id, id: favorite.id }
- 
+
+       delete :destroy, { post_id: @post.id, topic_id: @post.topic.id, id: favorite.id }
+
        expect( @user.favorites.find_by_post_id(@post.id) ).to eq(nil)
      end
    end
- end
+end
