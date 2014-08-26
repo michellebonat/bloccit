9bfe060c38c0dd6d7032ab5867a14217b3cd5c16
michellebonat <michelle.bonat@gmail.com>
2014-08-26 13:46:03 -0700

    Added checking user permissions before emailing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,11 +7,17 @@ class Comment < ActiveRecord::Base
 
      after_create :send_favorite_emails
  
-   private
- 
-   def send_favorite_emails
-     post.favorites.each do |favorite|
-       FavoriteMailer.new_comment(favorite.user, post, self).deliver
-     end
+ private
+
+ def send_favorite_emails
+   post.favorites.each do |favorite|
+     if should_receive_update_for?(favorite)
+       FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+     end       
    end
+ end
+
+ def should_receive_update_for?(favorite)
+   user_id != favorite.user_id && favorite.user.email_favorites?
+ end
 end
