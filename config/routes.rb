f4cbe5cf5ddebc6b7e09d7a0202f55559c9ad95f
michellebonat <michelle.bonat@gmail.com>
2014-08-27 13:18:01 -0700

    Addind popular posts and refactoring post and topic nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Bloccit::Application.routes.draw do
   get "comments/create"
   devise_for :users
-  resources :users, only: [:update, :show]
-  resources :posts
+  resources :users, only: [:update, :show, :index]
+  resources :topics
 
-   
+  resources :topics do
+      resources :posts, except: [:index], controller: 'topics/posts'
+  end
 
-
-
-   resources :topics
-
-    resources :topics do
-      resources :posts, except: [:index] do
-        resources :comments, only: [:create, :destroy]
-        resources :favorites, only: [:create, :destroy]
-        post '/up-vote' => 'votes#up_vote', as: :up_vote
-        post '/down-vote' => 'votes#down_vote', as: :down_vote
-      end  
-    end
+  resources :posts, only: [:index] do
+    resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote 
+  end
     
   get 'about' => 'welcome#about'
 
-   root to: 'welcome#index'
+  root to: 'welcome#index'
 end
