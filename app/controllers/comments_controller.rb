384eb70b3aba6f21ac54cfc9168a507a9723a9a8
michellebonat <michelle.bonat@gmail.com>
2014-08-27 18:39:29 -0700

    Added part of Create Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,13 @@
 class CommentsController < ApplicationController
   respond_to :html, :js
+  
   def create
-    # this should create a new comment associated with a post and the current_user who created it
-    @topic = Topic.find( params[:topic_id] )
-    @post = @topic.posts.find( params[:post_id] )
-    @comments = @post.comments
-
-    @comment = current_user.comments.build( comment_params )
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
     @comment.post = @post
     @new_comment = Comment.new
 
-    # authorize @comment
+    authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment was created."
@@ -18,10 +15,35 @@ class CommentsController < ApplicationController
       flash[:error] = "There was an error saving the comment. Please try again."
     end
 
-    respond_with(@comment) do |f|
-      f.html { redirect_to [@topic, @post] }
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
+
+# this is the create method I had before the exersice told me to replace it in the Create Ajax exercise
+
+ # def create
+    # this should create a new comment associated with a post and the current_user who created it
+  #  @topic = Topic.find( params[:topic_id] )
+  #  @post = @topic.posts.find( params[:post_id] )
+  #  @comments = @post.comments
+
+  #  @comment = current_user.comments.build( comment_params )
+  #  @comment.post = @post
+  #  @new_comment = Comment.new
+
+    # authorize @comment
+
+  #  if @comment.save
+  #    flash[:notice] = "Comment was created."
+  #  else
+  #    flash[:error] = "There was an error saving the comment. Please try again."
+  #  end
+
+  #  respond_with(@comment) do |f|
+  #    f.html { redirect_to [@topic, @post] }
+  #  end
+ # end
 
   def destroy
     @topic = Topic.find(params[:topic_id])
@@ -40,10 +62,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  private
+
   def comment_params
-    params.require(:comment).permit(
-      :body,
-      :post_id
-    )
+    params.require(:comment).permit(:body)
   end
-end
+
+# this is the comment_params I had before the Create Ajax had me update it I did not have as private
+#  def comment_params
+#    params.require(:comment).permit(
+#      :body,
+#      :post_id
+#    )
+#  end
+#end
