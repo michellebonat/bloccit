8da6ff9f74e0dc35793964aa1a4550c63cffc928
michellebonat <michelle.bonat@gmail.com>
2014-08-28 11:17:41 -0700

    Fixes for links and image uploads

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,66 +1,67 @@
 class TopicsController < ApplicationController
   def index
-     @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10)
-     authorize @topics
+    @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10)
+    authorize @topics
   end
 
   def new
-     @topic = Topic.new
-     authorize @topic
+    @topic = Topic.new
+    authorize @topic
   end
 
   def show
-     @topic = Topic.find(params[:id])
-     authorize @topic
-     @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
   end
 
   def edit
-     @topic = Topic.find(params[:id])
-     authorize @topic
+    @topic = Topic.find(params[:id])
+    authorize @topic
   end
- 
-   def create
+
+  def create
     # changed this next line for interlude refactor for params
-     @topic = Topic.new(topic_params)
-     authorize @topic
-     if @topic.save
-       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash[:error] = "Error creating topic. Please try again."
-       render :new
-     end
-   end
- 
+    @topic = Topic.new(topic_params)
+    authorize @topic
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash[:error] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
   def destroy
-     @topic = Topic.find(params[:id])
-     name = @topic.name
- 
-     authorize @topic
-     if @topic.destroy
-       flash[:notice] = "\"#{name}\" was deleted successfully."
-       redirect_to topics_path
-     else
-       flash[:error] = "There was an error deleting the topic."
-       render :show
-     end
-   end
+    @topic = Topic.find(params[:id])
+    name = @topic.name
 
-   def update
-     @topic = Topic.find(params[:id])
-     authorize @topic
-     # chamged this next line for interlude 34 refactor for params
-     if @topic.update_attributes(topic_params) 
-       redirect_to @topic
-     else
-       flash[:error] = "Error saving topic. Please try again"
-       render :edit
-     end
-   end
+    authorize @topic
+    if @topic.destroy
+      flash[:notice] = "\"#{name}\" was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :show
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    # chamged this next line for interlude 34 refactor for params
+    if @topic.update_attributes(topic_params)
+      redirect_to @topic
+    else
+      flash[:error] = "Error saving topic. Please try again"
+      render :edit
+    end
+  end
+
 # this is refactor for Interlude exercise not sure if correct
-   private
+  private
 
-   def topic_params
+  def topic_params
     params.require(:topic).permit(:name, :description, :public)
-   end
+  end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -34,20 +34,20 @@ class AvatarUploader < CarrierWave::Uploader::Base
   version :tiny do
     process resize_to_fill: [20, 20]
   end
- 
+
   version :small do
     process resize_to_fill: [30, 30]
   end
- 
+
   version :profile do
     process resize_to_fill: [45, 45]
-  end  
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
